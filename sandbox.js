e6088363dd229b33362b3d13121682142ce9888b
Safei Ashraf <safei.ashraf@gmail.com>
2022-08-08 23:15:37 +0200

    basic function

---
+++ sandbox.js
@@ -0,0 +1,7 @@
+var character = 'Mario';
+var age = 30;
+var isBlackBelt = false;
+var circleDiameter = function (diameter) {
+    return diameter * Math.PI;
+};
+console.log(circleDiameter(12));
---
+++ sandbox.ts
@@ -0,0 +1,10 @@
+let character = 'Mario';
+let age = 30;
+let isBlackBelt = false;
+
+
+const circleDiameter = (diameter: number): number =>{
+    return diameter * Math.PI;
+}
+
+console.log(circleDiameter(12))
