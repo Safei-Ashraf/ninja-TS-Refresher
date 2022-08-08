06b61b6eb11b480e3e0775798b0f81c548a8dbd0
Safei Ashraf <safei.ashraf@gmail.com>
2022-08-09 01:42:16 +0200

    interfaces

--- public/app.js
+++ public/app.js
@@ -1,26 +1,20 @@
 "use strict";
 // const anchor = document.querySelector('a')!;
 // console.log(anchor.href);
-// const form = document.querySelector('.new-item-form') as HTMLFormElement;
-// console.log(form.children)
-class Invoice {
-    constructor(client, details, amount) {
-        this.client = client;
-        this.details = details;
-        this.amount = amount;
-    }
-    format() {
-        return `${this.client} owes  $${this.details} for  ${this.amount}`;
-    }
-}
-const invOne = new Invoice('mario', 'work on website', 250);
-console.log(invOne.amount);
+const magicalInvoice = {
+    client: 'magical',
+    details: '  magical invoice details',
+    amount: 2,
+    format: (input) => input,
+};
+console.log(magicalInvoice);
+console.log(magicalInvoice.format('test'));
 const formatInvoice = ({ client, details, amount }) => {
     return `${client} owes  $${details} for  ${amount}`;
 };
 const invTwo = formatInvoice({ client: 'luigi', details: 'work on website', amount: 9000 });
 console.log('inv two', invTwo);
-console.log('inv one', invOne);
+// console.log('inv one',invOne)
 const form = document.querySelector('.new-item-form');
 const type = document.querySelector('#type');
 const toFrom = document.querySelector('#tofrom');
--- src/app.ts
+++ src/app.ts
@@ -1,25 +1,53 @@
+
 // const anchor = document.querySelector('a')!;
 // console.log(anchor.href);
 
 // const form = document.querySelector('.new-item-form') as HTMLFormElement;
 // console.log(form.children)
 
-class Invoice {
+interface IsInvoice {
     client: string;
     details: string;
-    private amount: number;
-
-    constructor(client: string, details: string, amount: number) {
-        this.client = client;
-        this.details= details;
-        this.amount = amount;
-    }
-    format(){
-        return `${this.client} owes  $${this.details} for  ${this.amount}`;
-    }
+    amount: number;
+    format: (input : string) => string;
 }
-const invOne = new Invoice( 'mario', 'work on website', 250);
-console.log(invOne.amount);
+
+const magicalInvoice: IsInvoice ={
+    client: 'magical',
+    details: '  magical invoice details',
+    amount: 2,
+    format: (input : string) => input,
+}
+console.log(magicalInvoice)
+console.log(magicalInvoice.format('test'))
+
+
+
+
+
+
+
+
+
+
+
+
+// class Invoice {
+//     client: string;
+//     details: string;
+//     private amount: number;
+
+//     constructor(client: string, details: string, amount: number) {
+//         this.client = client;
+//         this.details= details;
+//         this.amount = amount;
+//     }
+//     format(){
+//         return `${this.client} owes  $${this.details} for  ${this.amount}`;
+//     }
+// }
+// const invOne = new Invoice( 'mario', 'work on website', 250);
+// console.log(invOne.amount);
 
 type TInvoice = {
     client: string;
@@ -31,7 +59,7 @@ const formatInvoice = ({client, details, amount}: TInvoice):string => {
 }
 const invTwo = formatInvoice({client:'luigi',details: 'work on website',amount: 9000})
 console.log('inv two',invTwo)
-console.log('inv one',invOne)
+// console.log('inv one',invOne)
 
 const form = document.querySelector('.new-item-form') as HTMLFormElement;
 
