f2db806c61e027e5e7ddb2a1c6c9a5dc2bf26ab1
Safei Ashraf <safei.ashraf@gmail.com>
2022-08-09 02:09:36 +0200

    generic

--- public/app.js
+++ public/app.js
@@ -1,26 +1,61 @@
 "use strict";
 // const anchor = document.querySelector('a')!;
 // console.log(anchor.href);
-const magicalInvoice = {
-    client: 'magical',
-    details: '  magical invoice details',
-    amount: 2,
-    format: (input) => input,
-};
-console.log(magicalInvoice);
-console.log(magicalInvoice.format('test'));
-const formatInvoice = ({ client, details, amount }) => {
-    return `${client} owes  $${details} for  ${amount}`;
-};
-const invTwo = formatInvoice({ client: 'luigi', details: 'work on website', amount: 9000 });
-console.log('inv two', invTwo);
+// const form = document.querySelector('.new-item-form') as HTMLFormElement;
+// console.log(form.children)
+// interface IsInvoice {
+//     client: string;
+//     details: string;
+//     amount: number;
+//     format: (input : string) => string;
+// }
+// const magicalInvoice: IsInvoice ={
+//     client: 'magical',
+//     details: '  magical invoice details',
+//     amount: 2,
+//     format: (input : string) => input,
+// }
+// console.log(magicalInvoice)
+// console.log(magicalInvoice.format('test'))
+// class Invoice {
+//     client: string;
+//     details: string;
+//     private amount: number;
+//     constructor(client: string, details: string, amount: number) {
+//         this.client = client;
+//         this.details= details;
+//         this.amount = amount;
+//     }
+//     format(){
+//         return `${this.client} owes  $${this.details} for  ${this.amount}`;
+//     }
+// }
+// const invOne = new Invoice( 'mario', 'work on website', 250);
+// console.log(invOne.amount);
+// type TInvoice = {
+//     client: string;
+//     details: string;
+//     amount: number;
+// }
+// const formatInvoice = ({client, details, amount}: TInvoice):string => {
+//     return `${client} owes  $${details} for  ${amount}`;
+// }
+// const invTwo = formatInvoice({client:'luigi',details: 'work on website',amount: 9000})
+// console.log('inv two',invTwo)
 // console.log('inv one',invOne)
-const form = document.querySelector('.new-item-form');
-const type = document.querySelector('#type');
-const toFrom = document.querySelector('#tofrom');
-const details = document.querySelector('#details');
-const amount = document.querySelector('#amount');
-form.addEventListener('submit', (e) => {
-    e.preventDefault();
-    console.log(type.value, toFrom.value, amount.value);
-});
+// const form = document.querySelector('.new-item-form') as HTMLFormElement;
+// const type = document.querySelector('#type') as HTMLSelectElement;
+// const toFrom = document.querySelector('#tofrom') as HTMLInputElement;
+// const details = document.querySelector('#details') as HTMLInputElement;
+// const amount = document.querySelector('#amount') as HTMLInputElement;
+//  form.addEventListener('submit', (e: Event) => {
+//     e.preventDefault();
+//     console.log(type.value, toFrom.value, amount.value)
+// });
+//Generics is a feature to alows us to create reusable blocks of code that we can use with different types.
+const addUID = (obj) => {
+    let uid = Math.floor(Math.random() * 100);
+    return Object.assign(Object.assign({}, obj), { uid });
+};
+let docOne = addUID({ name: 'ninja', age: 40 });
+console.log(docOne.name);
--- src/app.ts
+++ src/app.ts
@@ -5,21 +5,21 @@
 // const form = document.querySelector('.new-item-form') as HTMLFormElement;
 // console.log(form.children)
 
-interface IsInvoice {
-    client: string;
-    details: string;
-    amount: number;
-    format: (input : string) => string;
-}
+// interface IsInvoice {
+//     client: string;
+//     details: string;
+//     amount: number;
+//     format: (input : string) => string;
+// }
 
-const magicalInvoice: IsInvoice ={
-    client: 'magical',
-    details: '  magical invoice details',
-    amount: 2,
-    format: (input : string) => input,
-}
-console.log(magicalInvoice)
-console.log(magicalInvoice.format('test'))
+// const magicalInvoice: IsInvoice ={
+//     client: 'magical',
+//     details: '  magical invoice details',
+//     amount: 2,
+//     format: (input : string) => input,
+// }
+// console.log(magicalInvoice)
+// console.log(magicalInvoice.format('test'))
 
 
 
@@ -49,26 +49,37 @@ console.log(magicalInvoice.format('test'))
 // const invOne = new Invoice( 'mario', 'work on website', 250);
 // console.log(invOne.amount);
 
-type TInvoice = {
-    client: string;
-    details: string;
-    amount: number;
-}
-const formatInvoice = ({client, details, amount}: TInvoice):string => {
-    return `${client} owes  $${details} for  ${amount}`;
-}
-const invTwo = formatInvoice({client:'luigi',details: 'work on website',amount: 9000})
-console.log('inv two',invTwo)
+// type TInvoice = {
+//     client: string;
+//     details: string;
+//     amount: number;
+// }
+// const formatInvoice = ({client, details, amount}: TInvoice):string => {
+//     return `${client} owes  $${details} for  ${amount}`;
+// }
+// const invTwo = formatInvoice({client:'luigi',details: 'work on website',amount: 9000})
+// console.log('inv two',invTwo)
 // console.log('inv one',invOne)
 
-const form = document.querySelector('.new-item-form') as HTMLFormElement;
+// const form = document.querySelector('.new-item-form') as HTMLFormElement;
 
-const type = document.querySelector('#type') as HTMLSelectElement;
-const toFrom = document.querySelector('#tofrom') as HTMLInputElement;
-const details = document.querySelector('#details') as HTMLInputElement;
-const amount = document.querySelector('#amount') as HTMLInputElement;
+// const type = document.querySelector('#type') as HTMLSelectElement;
+// const toFrom = document.querySelector('#tofrom') as HTMLInputElement;
+// const details = document.querySelector('#details') as HTMLInputElement;
+// const amount = document.querySelector('#amount') as HTMLInputElement;
 
- form.addEventListener('submit', (e: Event) => {
-    e.preventDefault();
-    console.log(type.value, toFrom.value, amount.value)
-});
+//  form.addEventListener('submit', (e: Event) => {
+//     e.preventDefault();
+//     console.log(type.value, toFrom.value, amount.value)
+// });
+
+//Generics is a feature to alows us to create reusable blocks of code that we can use with different types.
+
+const addUID = <T extends {name : string}>( obj: T)=>{
+    let uid = Math.floor(Math.random() * 100);
+    return {...obj, uid};
+
+}
+
+let docOne = addUID({name: 'ninja', age: 40});
+console.log(docOne.name)
