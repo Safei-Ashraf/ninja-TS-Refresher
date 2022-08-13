685199a021098ceab60b37d298e221b0701491ed
Safei Ashraf <safei.ashraf@gmail.com>
2022-08-13 19:31:19 +0200

    adding p1

--- src/app.ts
+++ src/app.ts
@@ -1,39 +1,65 @@
-// //object type annotation
+// // //object type annotation
 
-// let myObject :{
-//    readonly name: string;
-//     age: number;
-//     hire?: boolean;
-// } = {
-//     name: "John",
-//     age: 32,    
-//     hire: false
-// };
+// // let myObject :{
+// //    readonly name: string;
+// //     age: number;
+// //     hire?: boolean;
+// // } = {
+// //     name: "John",
+// //     age: 32,    
+// //     hire: false
+// // };
 
 
-// // myObject.name = "John";
+// // // myObject.name = "John";
 
-//Interface Declaration:
-interface User{
-    id?: number;
+// //Interface Declaration:
+// interface User{
+//     id?: number;
+//     username: string;
+//     country: string;
+//     sayWelcome: ()=> string;
+//     sayHello(): string;
+//     getDouble: (number: number) => number;
+// }
+
+// const user: User = {
+//     id: 1,
+//     username: "john",
+//     country: "USA",
+//     getDouble(n){
+//         return n*2;
+//     },
+//     sayHello() {
+//         return "Hello" + this.username;
+//     },
+//     sayWelcome: ()=>{
+//         return "Welcome" + user.username;
+//     }
+// }
+
+//Week 3 Problems:
+//1-
+// Edit The Interface To Fix The Problems
+interface Member {
+    id: number;
     username: string;
     country: string;
-    sayWelcome: ()=> string;
-    sayHello(): string;
-    getDouble: (number: number) => number;
-}
-
-const user: User = {
-    id: 1,
-    username: "john",
-    country: "USA",
-    getDouble(n){
-        return n*2;
-    },
-    sayHello() {
-        return "Hello" + this.username;
-    },
-    sayWelcome: ()=>{
-        return "Welcome" + user.username;
+    readonly state: boolean;
+    getName: () => string;
+  }
+  
+  // Do Not Edit The Code Below
+  let user: Member = { // Property 'country' is missing in type
+    id: 100,
+    username: "Elzero",
+    state: true,
+    getName() { // 'getName' does not exist in type 'Member'
+      return this.username;
     }
-}
+  }
+  
+  user.id = 200;
+  user.id = "200"; // Type 'string' is not assignable to type 'number'
+  user.state = false; // Cannot assign to 'state' because it is a read-only property
+  
