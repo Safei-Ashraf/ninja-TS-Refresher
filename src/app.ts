46b4d5f7d4834b0918aaae02a54cf5b30b3ba447
Safei Ashraf <safei.ashraf@gmail.com>
2022-08-13 17:51:47 +0200

    interfaces with functions

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,39 @@
-//object type annotation
+// //object type annotation
 
-let myObject :{
-   readonly name: string;
-    age: number;
-    hire?: boolean;
-} = {
-    name: "John",
-    age: 32,    
-    hire: false
-};
+// let myObject :{
+//    readonly name: string;
+//     age: number;
+//     hire?: boolean;
+// } = {
+//     name: "John",
+//     age: 32,    
+//     hire: false
+// };
 
 
-// myObject.name = "John";
+// // myObject.name = "John";
+
+//Interface Declaration:
+interface User{
+    id?: number;
+    username: string;
+    country: string;
+    sayWelcome: ()=> string;
+    sayHello(): string;
+    getDouble: (number: number) => number;
+}
+
+const user: User = {
+    id: 1,
+    username: "john",
+    country: "USA",
+    getDouble(n){
+        return n*2;
+    },
+    sayHello() {
+        return "Hello" + this.username;
+    },
+    sayWelcome: ()=>{
+        return "Welcome" + user.username;
+    }
+}
