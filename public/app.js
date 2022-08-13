2c7c481d21cdc28656c497338553811d0a85d2a9
Safei Ashraf <safei.ashraf@gmail.com>
2022-08-13 20:18:33 +0200

    p3 solution

--- public/app.js
+++ public/app.js
@@ -1,38 +1,24 @@
 "use strict";
 // // //object type annotation
 // Do Not Edit The Code Below
-let user = {
+let client = {
     id: 100,
     username: "Elzero",
-    state: true,
-    getName() {
-        return this.username;
+    active: true,
+    discount: 10,
+    getPrice(price) {
+        return price - this.discount;
     }
 };
-user.id = 200;
-user.id = "200"; // Type 'string' is not assignable to type 'number'
-user.state = false; // Cannot assign to 'state' because it is a read-only property
-//   //p-2
-//   // Create Interface Here
-// // interface Client{
-// //     id: number,
-// //     username: string,
-// //     active: boolean,
-// //     discount: number,
-// //     getPrice: (price: number) => number,
-// // }
-// // // Do Not Edit The Code Below
-// // let client: Client = {
-// //     id: 100,
-// //     username: "Elzero",
-// //     active: true,
-// //     discount: 10,
-// //     getPrice(price: number) {
-// //       return price - this.discount;
-// //     }
-// //   }
-// //   console.log(`Client Id Is ${client.id}`);
-// //   console.log(`Client Name Is ${client.username}`);
-// //   console.log(`Client Has Dicount ${client.discount}`);
-// //   console.log(`Client Product After Discount Is ${client.getPrice(200)}`);
-// // console.log('hey')
+console.log(`Client Id Is ${client.id}`);
+console.log(`Client Name Is ${client.username}`);
+console.log(`Client Has Dicount ${client.discount}`);
+console.log(`Client Product After Discount Is ${client.getPrice(200)}`);
+let creature = {
+    title: "Superman",
+    weight: 100,
+    age: 500,
+    canFly: true,
+    bodyType: "Iron",
+    origin: "Krypton"
+};
--- src/app.ts
+++ src/app.ts
@@ -1,70 +1,70 @@
-// // //object type annotation
+// // // //object type annotation
 
-// // let myObject :{
-// //    readonly name: string;
-// //     age: number;
-// //     hire?: boolean;
-// // } = {
-// //     name: "John",
-// //     age: 32,    
-// //     hire: false
-// // };
+// // // let myObject :{
+// // //    readonly name: string;
+// // //     age: number;
+// // //     hire?: boolean;
+// // // } = {
+// // //     name: "John",
+// // //     age: 32,    
+// // //     hire: false
+// // // };
 
 
-// // // myObject.name = "John";
+// // // // myObject.name = "John";
 
-// //Interface Declaration:
-// interface User{
-//     id?: number;
-//     username: string;
-//     country: string;
-//     sayWelcome: ()=> string;
-//     sayHello(): string;
-//     getDouble: (number: number) => number;
-// }
+// // //Interface Declaration:
+// // interface User{
+// //     id?: number;
+// //     username: string;
+// //     country: string;
+// //     sayWelcome: ()=> string;
+// //     sayHello(): string;
+// //     getDouble: (number: number) => number;
+// // }
 
-// const user: User = {
-//     id: 1,
-//     username: "john",
-//     country: "USA",
-//     getDouble(n){
-//         return n*2;
-//     },
-//     sayHello() {
-//         return "Hello" + this.username;
-//     },
-//     sayWelcome: ()=>{
-//         return "Welcome" + user.username;
-//     }
-// }
+// // const user: User = {
+// //     id: 1,
+// //     username: "john",
+// //     country: "USA",
+// //     getDouble(n){
+// //         return n*2;
+// //     },
+// //     sayHello() {
+// //         return "Hello" + this.username;
+// //     },
+// //     sayWelcome: ()=>{
+// //         return "Welcome" + user.username;
+// //     }
+// // }
 
-//Week 3 Problems:
-//1-
-// Edit The Interface To Fix The Problems
-// interface Member {
-//     id: number | string;
-//     username: string;
-//     country?: string;
-//      state: boolean;
-//     getName: () => string;
-//   }
+// //Week 3 Problems:
+// //1-
+// // Edit The Interface To Fix The Problems
+// // interface Member {
+// //     id: number | string;
+// //     username: string;
+// //     country?: string;
+// //      state: boolean;
+// //     getName: () => string;
+// //   }
   
-//   // Do Not Edit The Code Below
-//   let user: Member = { // Property 'country' is missing in type
-//     id: 100,
-//     username: "Elzero",
-//     state: true,
-//     getName() { // 'getName' does not exist in type 'Member'
-//       return this.username;
-//     }
-//   }
+// //   // Do Not Edit The Code Below
+// //   let user: Member = { // Property 'country' is missing in type
+// //     id: 100,
+// //     username: "Elzero",
+// //     state: true,
+// //     getName() { // 'getName' does not exist in type 'Member'
+// //       return this.username;
+// //     }
+// //   }
   
-//   user.id = 200;
-//   user.id = "200"; // Type 'string' is not assignable to type 'number'
-//   user.state = false; // Cannot assign to 'state' because it is a read-only property
+// //   user.id = 200;
+// //   user.id = "200"; // Type 'string' is not assignable to type 'number'
+// //   user.state = false; // Cannot assign to 'state' because it is a read-only property
   
-  //p-2
-  // Create Interface Here
+//   //p-2
+//   // Create Interface Here
 
 // interface Client{
 //     id: number;
@@ -88,3 +88,32 @@
 //   console.log(`Client Name Is ${client.username}`);
 //   console.log(`Client Has Dicount ${client.discount}`);
 //   console.log(`Client Product After Discount Is ${client.getPrice(200)}`);
+
+
+// //p-3
+
+// // Do Not Edit The Code Below
+// interface Man {
+//     title: string;
+//     weight: number;
+//     age: number;
+//   }
+  
+//   interface Bird {
+//     canFly: boolean;
+//   }
+
+//   interface Superman extends Man, Bird{
+//     bodyType: string;
+//     origin: string;
+//   }
+  
+//   let creature: Superman = {
+//     title: "Superman",
+//     weight: 100,
+//     age: 500,
+//     canFly: true,
+//     bodyType: "Iron",
+//     origin: "Krypton"
+//   }
+
